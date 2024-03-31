a94eebd367367c85e0fc7d2d738febe6fd8a0a12
leeseonhee17 <leeseonhee17@gmail.com>
2024-04-01 01:16:43 +0900

    링크에서 버튼만들기

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,14 +4,13 @@ import profilePic from "../../public/images/casamila.jpeg"
 
 export default function About(){
     return (
-        <div className="flex -space-x-1 overflow-hidden justify-center">
+        <div className="flex -space-x-1 overflow-hidden justify-center mt-10">
             <Image className="inline-block h-13 w-13 rounded-full ring-2 ring-white"
                     src={profilePic}
                     alt="casamila" 
                     width={500}
                     height={500}/>
-            <span className="text-lg text-pretty inline-block">Welcome to the official explorer for The New York Times Bestseller list explorer:
-                                        We hope enjoy your stay!</span>        
+            <p className="text-lg text-pretty inline-block mt-10">Welcome to the official explorer for The New York Times Bestseller list explorer:  We hope enjoy your stay!</p> 
         </div>
     );
 }
--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -5,8 +5,8 @@ import { IBook } from "../list/[id]/page"
 export function Book({book} :{book: IBook}) {
     return (
      
-      <Link href={book.amazon_product_url} className="group">
-            
+      
+            <div>
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <Image
                   src={book.book_image}
@@ -18,12 +18,14 @@ export function Book({book} :{book: IBook}) {
                 <span className="inline-flex items-center rounded-lg px-2 py-1 text-5xl font-lg text-black-900">
                   {book.rank}
                 </span>
-              </div>
+                <Link href={book.amazon_product_url} className="group color box-border" >
+                   Buy Now
+                </Link>
+               </div>
               <h3 className="mt-4 text-sm text-gray-700">{book.title}  /  {book.author}</h3>
               <p className="mt-1 text-lg font-medium text-gray-900">{book.price}</p>
-             {/*  <p className="mt-1 text-lg font-medium text-gray-900">{book.price}</p>
-              <p className="mt-1 text-lg font-medium text-gray-900">{book.price}</p> */}
-      </Link>
+            </div>    
+     
     
     )
 } 
