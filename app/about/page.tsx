1cc794decc1b6cac556d459b2aa2d4d1787e224b
leeseonhee17 <leeseonhee17@gmail.com>
2024-04-01 01:01:11 +0900

    어바웃수정 사진 글 추가

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import profilePic from "../../public/images/casamila.jpeg"
+import Sans
+
+export default function About(){
+    return (
+        <div className="flex -space-x-1 overflow-hidden justify-center">
+            <Image className="inline-block h-13 w-13 rounded-full ring-2 ring-white"
+                    src={profilePic}
+                    alt="casamila" 
+                    width={500}
+                    height={500}/>
+            <span className="text-lg text-pretty inline-block">Welcome to the official explorer for The New York Times Bestseller list explorer:
+                                        We hope enjoy your stay!</span>        
+        </div>
+    );
+}
--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -6,9 +6,7 @@ export function Book({book} :{book: IBook}) {
     return (
      
       <Link href={book.amazon_product_url} className="group">
-            <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-lg font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-              {book.rank}
-            </span>
+            
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <Image
                   src={book.book_image}
@@ -17,6 +15,9 @@ export function Book({book} :{book: IBook}) {
                   height={book.book_image_height}
                   className="h-full w-full object-cover object-center group-hover:opacity-75"
                 />
+                <span className="inline-flex items-center rounded-lg px-2 py-1 text-5xl font-lg text-black-900">
+                  {book.rank}
+                </span>
               </div>
               <h3 className="mt-4 text-sm text-gray-700">{book.title}  /  {book.author}</h3>
               <p className="mt-1 text-lg font-medium text-gray-900">{book.price}</p>
